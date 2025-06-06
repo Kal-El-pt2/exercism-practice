6280a92ece2111721e2524df76a009104971ee60
Kal-El-pt2 <arivwalia@gmail.com>
2025-06-06 14:40:06 +0530

    fix: intializing logic refined with a rem_euclid and add_minutes improved

--- exercism-exercises/src/clock.rs
+++ exercism-exercises/src/clock.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, Eq)]
 pub struct Clock{
     hrs: i32,
     mins: i32,
@@ -5,32 +6,18 @@ pub struct Clock{
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
+        let total_minutes = (hours * 60 + minutes).rem_euclid(24 * 60);
         Clock{
-            hrs: hours,
-            mins: minutes,
+            hrs: total_minutes / 60,
+            mins: total_minutes % 60,
         }
     }
 
     pub fn add_minutes(&self, mins: i32) -> Self {
-        let total_mins = self.hrs*60 + self.mins + mins;
-        let mut hours = (total_mins/60)/24;
-        let mut minutes = total_mins%60;
-
-        if (minutes < 0) {
-            minutes += 60;
-            hours -= 1;
-        }
-        if (hours < 0) {
-            hours = (hours+24)%24;
-        }
-
-        Clock {
-            hrs: hours,
-            mins:minutes
-        }
+        Clock::new(self.hrs, self.mins + mins)
     }
 
     pub fn to_string(&self) -> String {
-        format!("{:02}:{:02}",self.hours,self.mins)
+        format!("{:02}:{:02}", self.hrs, self.mins)
     }
 }
